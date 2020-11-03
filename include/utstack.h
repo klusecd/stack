7852bbfd21adc26842473a6bc8bd84ad66f1193e
Kate <klusecd@gmail.com>
2020-11-03 23:53:15 +0300

    queue&tests

--- include/utstack.h
+++ include/utstack.h
@@ -1,12 +1,10 @@
-﻿#ifndef __TSTACK_H__
-#define __TSTACK_H__
-#pragma once 
+﻿#pragma once 
 
 #include <iostream>
 
 using namespace std;
 
-const int MAX_STACK_SIZE = 1000;
+const int MAX_SIZE = 1000;
 
 
 template <class ValType>
@@ -20,7 +18,7 @@ public:
 
 	TStack(int s = 10)
 	{
-		if ((s < 0) || (s > MAX_STACK_SIZE)) throw "Error";
+		if ((s < 0) || (s > MAX_SIZE)) throw "Error";
 		maxSize = s;
 		size = 0;
 		sStack = new ValType[maxSize];
@@ -52,17 +50,12 @@ TStack&  operator=(const TStack &s)
 
 bool operator==(const TStack &s) const
 {
-	bool isEqual = 1;
-	if ((size != s.size) || (maxSize != s.maxSize)) return 0;
-	for (int i = 0; i <= maxSize; i++)
+	if ((size != s.size) || (maxSize != s.maxSize)) return false;
+	for (int i = 0; i < maxSize; i++)
 	{
-		if (sStack[i] != s.sStack[i])
-		{
-			isEqual = 0;
-			break;
-		}
+		if (sStack[i] != s.sStack[i]) return false;
 	}
-	return isEqual;
+	return true;
 }
 
 bool TStack<ValType>::operator!=(const TStack &s) const
@@ -85,7 +78,7 @@ bool full()
 
 void push(ValType elem)
 {
-	if (size == maxSize)
+	if (full())
 	{
 		if (full()) throw "Stack full";
 		maxSize *= 2.0;
@@ -96,16 +89,89 @@ void push(ValType elem)
 	}
 
 	sStack[size] = elem;
-	size++;;
+	size++;
 }
 
 void pop()
 {
 	if (empty()) throw "Stack empty";
-	sStack[size]= '\0';
-	size--;
+	sStack[--size] = 0;
+}
+
+ValType top() {
+	if (empty()) throw "Stack empty";
+	return sStack[size - 1];
+}
+
+ValType operator[](int pos)
+{
+	if ((pos < 0) || (pos > maxSize)) throw "error";
+	return sStack[pos];
 }
 
 };
 
-#endif
+template <class ValType>
+
+class TQueue : public TStack<ValType>
+{
+private:
+	TStack<ValType> top, tail;
+	int indexLowEl;
+public:
+
+	TQueue(int s = 10) 
+	{
+		TStack <ValType> tmp(s);
+		top = tmp;
+		tail = tmp;
+		indexLowEl = 0;
+
+	}
+
+	~TQueue() {}
+
+	void pop() 
+	{
+		while (!top.empty())
+		{
+			tail.push(top.top());
+			top.pop();
+		}
+		tail.pop();
+
+		while (!tail.empty())
+		{
+			top.push(tail.top());
+			tail.pop();
+		}
+	}
+	                    
+	void push(ValType elem) 
+	{
+		top.push(elem);
+	}
+
+	bool operator==(const TQueue &q)
+		{
+			if ((!q.top.empty()) && (!top.empty()))
+				if (top == q.top) return true;
+			if ((!q.tail.empty()) && (!tail.empty()))
+				if (tail == q.tail) return true;
+			return false;
+		}
+
+	bool operator!=(const TQueue &q)
+	{
+		if (q == *this) return false;
+		return true;
+	}
+
+	ValType operator[](int pos) {
+	
+		if (tail.empty()) return top[pos + indexLowEl];
+		return tail[pos];
+
+	}
+
+};
--- test/test_tstack.cpp
+++ test/test_tstack.cpp
@@ -9,7 +9,7 @@ TEST(TStack, can_create_stack)
 
 TEST(TStack, cant_create_too_large_stack)
 {
-	ASSERT_ANY_THROW(TStack<int> s(MAX_STACK_SIZE + 1));
+	ASSERT_ANY_THROW(TStack<int> s(MAX_SIZE + 1));
 }
 
 TEST(TStack, cant_create_stack_with_negative_length)
@@ -49,4 +49,113 @@ TEST(TStack, throws_when_push_into_full_stack)
 	s.push(2);
 	s.push(3);
 	ASSERT_ANY_THROW(s.push(4));
+}
+
+TEST(TStack, can_get_top) {
+	TStack<int> s(3);
+	s.push(1);
+	ASSERT_NO_THROW(s.top());
+}
+
+TEST(TQueue, can_create_queue_with_positive_length)
+{
+	ASSERT_NO_THROW(TQueue<int> q(5));
+}
+
+TEST(TQueue, cant_create_too_large_queue)
+{
+	ASSERT_ANY_THROW(TQueue<int> q(MAX_SIZE + 1));
+}
+
+TEST(TQueue, throws_when_create_queue_with_negative_length)
+{
+	ASSERT_ANY_THROW(TQueue<int> q(-1));
+}
+
+TEST(Queue, cant_do_pop_when_queue_is_empty) {
+	TQueue<int> q;
+	ASSERT_ANY_THROW(q.pop());
+}
+
+TEST(TQueue, can_pop)
+{
+	TQueue<int> q(5);
+	q.push(1);
+	ASSERT_NO_THROW(q.pop());
+}
+
+TEST(TQueue, no_throw_when_many_pushes_and_pops)
+{
+	TQueue<int> q(5);
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	q.push(5);
+	q.pop();
+	q.push(6);
+	ASSERT_NO_THROW(q.pop());
+}
+
+TEST(TQueue, cannot_push_full_queue) {
+	TQueue<int> q(2);
+	q.push(2);
+	q.push(4);
+	ASSERT_ANY_THROW(q.push(3));
+}
+
+TEST(TQueue, can_create_queue)
+{
+	ASSERT_NO_THROW(TQueue<int> s);
+}
+
+TEST(TQueue, can_push)
+{
+	TQueue<int> q;
+	ASSERT_NO_THROW(q.push(1));
+}
+
+
+TEST(TQueue, throws_when_pop_from_empty_stack)
+{
+	TQueue<int> q;
+	ASSERT_ANY_THROW(q.pop());
+}
+
+
+TEST(TQueue, works_correctly)
+{
+	TQueue<int> q(5);
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	q.push(5);
+	q.pop();
+	q.push(6);
+	EXPECT_EQ(6, q[4]);
+}
+
+
+TEST(TQueue, can_return_element)
+{
+	TQueue<int> q(5);
+	q.push(1);
+	ASSERT_NO_THROW(q[0]);
+}
+
+TEST(TQueue, return_true_when_queue_is_full) {
+	TQueue<int> q;
+	EXPECT_EQ(false, q.full());
+}
+
+TEST(TQueue, return_false_when_queue_isnt_full) {
+	TQueue<int> q(6);
+	q.push(1);
+	EXPECT_EQ(false, q.full());
+}
+
+TEST(TQueue, return_true_when_queue_is_empty) {
+	TQueue<int> q;
+	EXPECT_EQ(true, q.empty());
 }
